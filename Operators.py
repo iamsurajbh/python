61d1a9e18fcc6a06faebb4ea6ac4571cbfa2ae99
Suraj Bhattarai <surajbhattarai339@gmail.com>
2018-04-22 08:45:47 +0545

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,4 @@
+#Operators || **,*,/,+,-
+a = 98-15+14*16**2 #So here ** will operate 1st and * will operate 2nd / will operate 3rd and same as simplification rule...
+print(a)
+#If u want to operate - 1st then others then u should do like this :: (98-15)+14*16**2
