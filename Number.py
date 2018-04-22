ff8d4824ac5c12157978289405dd537d85ebd0d3
Suraj Bhattarai <surajbhattarai339@gmail.com>
2018-04-22 08:48:55 +0545

    Delete Number.py

--- Number.py
+++
@@ -1,6 +0,0 @@
-#Some number types are:
-#Integer=8,9,5,6,100 | Float=8.1,9.2222,5555.62
-a = 8 #Here a and b are containers which store numbers...
-b = 7 
-print(a + b) #If u type print("a"+"b")...then it willnot give u 8+7=15 instead it will concacenate. "" are use to mention strings.
-
